ab788e6133df0e93a1b520889458ff96737937de
kelle oliveira <kellemonik@gmail.com>
2024-03-06 22:31:33 -0300

    mapa estilizado

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,19 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "mapbox-gl" # @3.1.2
 pin "process" # @2.0.1
+pin "@mapbox/mapbox-gl-geocoder", to: "@mapbox--mapbox-gl-geocoder.js" # @5.0.0
+pin "#lib/client.js", to: "#lib--client.js.js" # @0.13.7
+pin "@mapbox/fusspot", to: "@mapbox--fusspot.js" # @0.4.0
+pin "@mapbox/mapbox-sdk", to: "@mapbox--mapbox-sdk.js" # @0.13.7
+pin "@mapbox/mapbox-sdk/services/geocoding", to: "@mapbox--mapbox-sdk--services--geocoding.js" # @0.13.7
+pin "@mapbox/parse-mapbox-token", to: "@mapbox--parse-mapbox-token.js" # @0.2.0
+pin "base-64" # @0.1.0
+pin "eventemitter3" # @3.1.2
+pin "events" # @3.3.0
+pin "fuzzy" # @0.1.3
+pin "is-plain-obj" # @1.1.0
+pin "lodash.debounce" # @4.0.8
+pin "nanoid" # @3.3.7
+pin "subtag" # @0.5.0
+pin "suggestions" # @1.7.1
+pin "xtend" # @4.0.2
