b061703dea4378e5904b66847a138509de35b3bb
Gerson <gersonwacherski@gmail.com>
2024-03-09 16:19:00 -0300

    ok

--- db/migrate/20240309181756_remove_fk_from_favorite.rb
+++ db/migrate/20240309181756_remove_fk_from_favorite.rb
@@ -1,5 +1,5 @@
 class RemoveFkFromFavorite < ActiveRecord::Migration[7.1]
   def change
-    remove_reference :favorites, :roteiros, index: true, foreign_key: true
+    remove_reference :favorites, :roteiros
   end
 end
